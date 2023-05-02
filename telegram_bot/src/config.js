59d255f147ef1d5ed93522b3338897834287a7c4
Garin Yaroslav <garinyaroslav13@mail.ru>
2023-05-02 19:24:32 +0300

    1

--- telegram_bot/src/config.js
+++ telegram_bot/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    TOKEN: "6061128565:AAHUzhyJpvr3dmKvrpLfTgogNx65smO9CBQ", //you're personaly bot token
+    TOKEN: "###########################################", //you're personaly bot token
     DB_URL: "mongodb://0.0.0.0:27017", //localhost default
 };
